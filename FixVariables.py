db769525423eb3a6e657b10c6c71ed873f21577e
JFKproject <eblankenship1998@gmail.com>
2025-02-22 17:44:15 -0500

    Update FixVariables.py

--- FixVariables.py
+++ FixVariables.py
@@ -4,11 +4,11 @@
 ### The output should look like the follow line without the ###
 ### Hello, world. There were 10 cakes, but Lex Luthor stole 2. Now there's 8. And that's sad.
 
-the Hello = "Hello"
-the.World = "world"
+Hello = "Hello"
+World = "world"
 cakes = 10
 theft = 2
 
-remaining = cakes - THEFT
+remaining = cakes - theft
 
-print(f"{thehello}, {theWorld}. There were {10cakes} cakes, but Lex Luthor stole {theft}. Now there's {remaining}. And that's sad.")
+print(f"{Hello}, {World}. There were {cakes} cakes, but Lex Luthor stole {theft}. Now there's {remaining}. And that's sad.")
